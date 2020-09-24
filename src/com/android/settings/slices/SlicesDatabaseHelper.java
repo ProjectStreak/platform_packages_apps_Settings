d83e25ef82b4dff2e0c190b016e9beb533cddef5
maxwen <max.weninger@gmail.com>
2020-09-24 08:55:26 -0400

    Use Build.DATE for search index check
    Change-Id: I7199d9f812d8cb645f3abca815c62098248b53c4

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -235,6 +235,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.STREAK_FINGERPRINT;
+        return Build.DATE;
     }
 }
