Havoc-OS/android_packages_apps_Settings

Settings: Battery temp: Use MCC to determine the scale type
The whole tapping on the preference was cute but really not ideal. Most
users are one or the other and is usually based on where the hell they're
at. Using the MCC does just that.

Change-Id: I048cda9032a817035c32087cfd9ffeb49003d9d1

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -391,7 +391,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), false));
+                com.android.internal.util.havoc.Utils.mccCheck(getContext()) ?
+                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), false) + "°C");
     }
 
     @VisibleForTesting

ProjectStreak/platform_packages_apps_Settings

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -50,6 +50,7 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.app.LoaderManager.LoaderCallbacks;
 import androidx.loader.content.Loader;
 
+import com.android.internal.util.streak.StreakUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
@@ -372,7 +373,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.streak.Utils.batteryTemperature(getContext(), false));
+                com.android.internal.util.streak.StreakUtils.mccCheck(getContext()) ?
+                com.android.internal.util.streak.StreakUtils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.streak.StreakUtils.batteryTemperature(getContext(), false) + "°C");
 
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,