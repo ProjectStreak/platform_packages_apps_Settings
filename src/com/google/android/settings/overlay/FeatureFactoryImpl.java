21952791b0ccdb89f8bf08ce6375cd9df1f0b0f9
Anay Wadhera <anay1018@gmail.com>
2021-12-26 13:59:58 -0500

    Settings: Import GoogleGameDashboard
    Change-Id: I1ffec9253c5db0f424e813e854cba6e534d1a4ed

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,4 +1,19 @@
 package com.google.android.settings.overlay;
 
+import android.content.Context;
+
+import com.android.settings.applications.GameSettingsFeatureProvider;
+import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
+
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
+
+    private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
+
+    @Override
+    public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
+        if (mGameSettingsFeatureProvider == null) {
+            mGameSettingsFeatureProvider = new GameSettingsFeatureProviderGoogleImpl();
+        }
+        return mGameSettingsFeatureProvider;
+    }
 }
